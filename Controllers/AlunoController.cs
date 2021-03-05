7c804c1c71815178c95cd9f3dd950a3b822341ac
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-03-04 22:09:12 -0300

    subindo arquivo

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TP03.Models;
+
+namespace TP03.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Aluno aluno = new Aluno();
+            aluno.Nome = "Leandro Bento Bergamo";
+            aluno.Ra = "160548";
+            aluno.DataDeNascimento = new DateTime(1995,03,02);
+
+            return View(aluno);
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP03.Models
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Ra { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+    }
+}
